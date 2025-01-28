58ca6b3b0725cad123e169734fc43135a8993fd2
@mouhamedder98 <mouhamedder98@gmail.com>
2025-01-28 20:39:18 +0000

    user

---
+++ Documents/Nouveau dossier (2)/projetoperationelle/Nouveau dossier/userToken/src/main/java/com/example/demo/UserTokenApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UserTokenApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(UserTokenApplication.class, args);
+		
+	}
+    
+	
+	
+}
